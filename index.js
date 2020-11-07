f187e69052708535ed04091d5b8ffc27ebf555d0
oncomouse <oncomouse@gmail.com>
2020-11-07 12:46:58 -0600

    Output object

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 Array.from(document.querySelectorAll('.order-history-item')).map(
   function (item) {
-    console.log(item.querySelector('.order-history-item__name').innerText.split(/\n/)[0] + ' ' + parseInt(item.querySelector('.order-history-item__quantity').innerText.replace(/Qty: /, ''), 10));
+    return {
+      name: item.querySelector('.order-history-item__name').innerText.split(/\n/)[0],
+      quantity: parseInt(item.querySelector('.order-history-item__quantity').innerText.replace(/Qty: /, ''), 10)
+    };
   }
 );
