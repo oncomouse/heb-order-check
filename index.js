910ce099ba21a017760c7309f26968c52ca28d51
oncomouse <oncomouse@gmail.com>
2020-11-07 14:50:07 -0600

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-Array.from(document.querySelectorAll('.order-history-item')).map(
-  function (item) {
-    return {
-      name: item.querySelector('.order-history-item__name').innerText.split(/\n/)[0],
-      quantity: parseInt(item.querySelector('.order-history-item__quantity').innerText.replace(/Qty: /, ''), 10)
-    };
-  }
-);
+var KEY = 'heb_order_check';
+var PROMPT = 'Please enter the saved JSON file.';
+window.extractList = function () {
+  return Array.from(document.querySelectorAll('.order-history-item')).map(
+    function (item) {
+      return {
+        name: item.querySelector('.order-history-item__name').innerText.split(/\n/)[0],
+        quantity: parseInt(item.querySelector('.order-history-item__quantity').innerText.replace(/Qty: /, ''), 10)
+      };
+    }
+  );
+};
+window.previousListExists = function () {
+  return window.localStorage.getItem(KEY) !== null;
+};
+var currentOrder = window.extractList();
+var previousOrder = window.previousListExists() ? JSON.parse(window.localStorage.getItem(KEY)) : JSON.parse(window.prompt(PROMPT, '[]'));
